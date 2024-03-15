a165ce15f4e87a0ec75fc8bd92d8cd303f529613
Katarina Petrovic <katarinap35@gmail.com>
2024-03-15 16:43:14 +0100

    spans, animation, test complete

--- js/skript.js
+++ js/skript.js
@@ -12,4 +12,71 @@ function generateLines() {
  }
 }
 
-window.onload = generateLines;
+window.onload = function () {
+ generateLines();
+ start();
+};
+
+function start() {
+ let startBtn = $('.startBtn');
+ let mainInput = $('.mainInput');
+ let allLines = $('.line');
+
+ startBtn.on('click', startGame);
+
+ function startGame() {
+  $(this).hide();
+ }
+
+ // setup
+ let speed = 1;
+ let textLength = 3;
+ let typingWords = words.filter(word => word.length == textLength);
+ let lvl = 6;
+
+ // console.log(typingWords);
+
+ //insert spans
+ function insertSpans() {
+  for (let i = 0; i < allLines.length; i++) {
+   let rand = Math.floor(Math.random() * 20);
+   if (rand <= lvl) {
+    let text = chooseText();
+    $(allLines[i]).append(`<span>${text}</span>`)
+   }
+  }
+ }
+ insertSpans();
+
+ function chooseText() {
+  let rand = Math.floor(Math.random() * typingWords.length);
+  let savedText = typingWords[rand];
+  typingWords.splice(rand, 1);
+
+  return savedText;
+ }
+
+ // animation
+
+ let moveAll = setInterval(function () {
+  let allSpans = $('span');
+  allSpans.css({
+   left: '+=' + speed
+  })
+
+  // test
+  $.each(allSpans, (index, el) => {
+   let position = $(el).position().left;
+   if (position > 850) {
+    clearAllIntervals()
+   } else if (position > 700 && position < 710) {
+    $(el).addClass('danger')
+   }
+  })
+ }, 10)
+
+ function clearAllIntervals() {
+  clearInterval(moveAll);
+ }
+}
+
